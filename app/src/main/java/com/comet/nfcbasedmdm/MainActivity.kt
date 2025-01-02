9a4321ac5d7206e02addc55cfd2fd2efa244c463
choi-hyeseong <bd2845@kakao.com>
2025-01-02 13:30:42 +0900

    feat: add overlay, notification permission check

--- app/src/main/java/com/comet/nfcbasedmdm/MainActivity.kt
+++ app/src/main/java/com/comet/nfcbasedmdm/MainActivity.kt
@@ -3,12 +3,17 @@ package com.comet.nfcbasedmdm
 import android.app.ActivityManager
 import android.app.AppOpsManager
 import android.app.admin.DevicePolicyManager
-import android.content.*
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.net.Uri
 import android.nfc.NfcAdapter
 import android.nfc.cardemulation.CardEmulation
-import android.os.*
+import android.os.Build
+import android.os.Bundle
+import android.os.Process
 import android.provider.Settings
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
@@ -26,6 +31,10 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(), ActivityCallback {
 
+    companion object {
+        private const val NOTIFICATION_PERMISSION = "android.permission.POST_NOTIFICATIONS"
+    }
+
     private val viewModel: MainViewModel by viewModels()
 
     // 서비스 작동여부 확인용
@@ -34,8 +43,7 @@ class MainActivity : AppCompatActivity(), ActivityCallback {
     // 기기 관리자 권한 허용
     private val deviceAdminLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
         //result == 활성화 이후 작동 + 서비스 연결이후 액티비티 결과 반환
-        if (isAdminActivated())
-            checkPermissions() // 나머지 펄미션 확인
+        if (isAdminActivated()) checkPermissions() // 나머지 펄미션 확인
         else {
             Toast.makeText(this, R.string.device_admin_required, Toast.LENGTH_SHORT).show()
             finish()
@@ -43,13 +51,32 @@ class MainActivity : AppCompatActivity(), ActivityCallback {
     }
 
     // android 10 이상 권한 사용시간 권한 체크
-    private val overlayLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+    private val usageLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
         //사용시간 권한 활성화 체크
-        if (isUsageEnabled())
-            init()
+        if (isUsageEnabled()) checkPermissions() //나머지 펄미션 확인
         else {
             Toast.makeText(this, R.string.usage_required, Toast.LENGTH_SHORT).show()
+            finish()
         }
+    }
+
+    private val notificationLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+        if (isNotificationEnabled()) checkPermissions() //나머지 perm 확인
+        else {
+            Toast.makeText(this, R.string.notification_required, Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
+
+    // overlay - 강제 카메라 종료 펄미션 확인
+    private val overlayLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (isOverlayEnabled())
+            checkPermissions()
+        else {
+            Toast.makeText(this, getString(R.string.overlay_required), Toast.LENGTH_LONG).show()
+            finish()
+        }
+
     }
 
     private val policyManager: DevicePolicyManager by lazy { getSystemService(DEVICE_POLICY_SERVICE) as DevicePolicyManager }
@@ -63,13 +90,23 @@ class MainActivity : AppCompatActivity(), ActivityCallback {
         checkPermissions()
     }
 
+    // 분명 FLOATING TWITTER 제작할때는 post notification 안줬어도 잘떴는데, 왜 얘는 알림 권한 줘야 하는걸까..?
+    // 13부터는 필요..?
     private fun checkPermissions() {
         if (!isAdminActivated()) {
             deviceAdminLauncher.launch(Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN).putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, receiver))
             return
         }
         if (!isUsageEnabled()) {
-            overlayLauncher.launch(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
+            usageLauncher.launch(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
+            return
+        }
+        if (!isOverlayEnabled()) {
+            overlayLauncher.launch(Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName")))
+        }
+
+        if (!isNotificationEnabled()) {
+            notificationLauncher.launch(arrayOf(NOTIFICATION_PERMISSION))
             return
         }
         init()
@@ -77,13 +114,10 @@ class MainActivity : AppCompatActivity(), ActivityCallback {
     }
 
     private fun init() {
-        if (NfcAdapter.getDefaultAdapter(this) != null)
-            setupNFCInstance()
+        if (NfcAdapter.getDefaultAdapter(this) != null) setupNFCInstance()
         viewModel.existMDMData().observe(this) { isExist ->
-            if (isExist)
-                switchToMain()
-            else
-                switchToRegister()
+            if (isExist) switchToMain()
+            else switchToRegister()
         }
     }
 
@@ -92,21 +126,40 @@ class MainActivity : AppCompatActivity(), ActivityCallback {
     }
 
 
+    private fun isOverlayEnabled() : Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+            return Settings.canDrawOverlays(this)
+        else
+            true
+    }
+
     private fun isUsageEnabled(): Boolean {
         val appOps = getSystemService(APP_OPS_SERVICE) as AppOpsManager
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) AppOpsManager.MODE_ALLOWED == appOps.unsafeCheckOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, Process.myUid(), packageName)
         else return true //Q 미만일경우 굳이 할필요 없음.
+    }
+
+
+    private fun isNotificationEnabled(): Boolean {
+        // T 이상인경우 (13)
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+            checkSelfPermission(NOTIFICATION_PERMISSION) == PERMISSION_GRANTED
+        else
+            // 이전버젼은 그냥 허용
+            true
 
     }
+
 
     private fun setupNFCInstance() {
         val component = ComponentName(this, HostService::class.java)
         val adapter = NfcAdapter.getDefaultAdapter(this)
         val dynamic = arrayListOf("F239856324897348")
-        CardEmulation.getInstance(adapter).also { it.registerAidsForService(component, "other", dynamic) }
+        CardEmulation.getInstance(adapter)
+            .also { it.registerAidsForService(component, "other", dynamic) }
     }
 
-    private fun isServiceRunning(serviceClass: Class<*>) : Boolean {
+    private fun isServiceRunning(serviceClass: Class<*>): Boolean {
         return activityManager.getRunningServices(Int.MAX_VALUE)
             .find { it.service.className == serviceClass.name } != null
     }
@@ -114,8 +167,7 @@ class MainActivity : AppCompatActivity(), ActivityCallback {
     override fun switchToMain() {
         switch(MainFragment(), false)
         // 메인화면 전환시에 서비스 실행. << mdm 초기화전까지는 작동되선 안됨
-        if (!isServiceRunning(MdmService::class.java))
-            startForegroundService(Intent(this, MdmService::class.java))
+        if (!isServiceRunning(MdmService::class.java)) startForegroundService(Intent(this, MdmService::class.java))
     }
 
     override fun switchToRegister() {
